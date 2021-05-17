1af562deb343b86d7528e3fdcf44c780633b0bb8
Kunal Tanwar <kunaltanwar2@gmail.com>
2021-05-17 17:41:18 +0530

    Fixed Typo in App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import styled from "styled-components";
-import ThePerfectButton from "./ThePerfectButton";
+import styled from 'styled-components';
+import Wrapper from './ThePerfectButton';
 
-const StyledButton = styled(ThePerfectButton)`
-  margin-bottom: 1rem;
-  min-width: 208px;
+const StyledButton = styled(Wrapper)`
+      margin-bottom: 1rem;
+      min-width: 208px;
 
-  &:last-child {
-    margin: 0;
-  }
+      &:last-child {
+            margin: 0;
+      }
 `;
 
 function App() {
-  return (
-    <>
-      <StyledButton>Primary</StyledButton>
-      <StyledButton isLoading>Loading</StyledButton>
-      <StyledButton isCompleted>
-        <span className="material-icons" style={{ marginRight: "8px" }}>
-          verified
-        </span>{" "}
-        Loaded
-      </StyledButton>
-      <StyledButton disabled>Disabled</StyledButton>
-    </>
-  );
+      return (
+            <>
+                  <StyledButton>Primary</StyledButton>
+                  <StyledButton isLoading>Loading</StyledButton>
+                  <StyledButton isCompleted>
+                        <span className="material-icons" style={{ marginRight: '8px' }}>
+                              verified
+                        </span>
+                        Loaded
+                  </StyledButton>
+                  <StyledButton disabled>Disabled</StyledButton>
+            </>
+      );
 }
 
 export default App;
